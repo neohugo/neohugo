a278a68cadaadc495f49b1d6bfdb77e5a270eea7
Prachya Saechua <blackb1rd.mov@gmail.com>
2020-10-31 19:22:37 +0700

    Feat/new benchmark script (#23)
    * feat: new benchmark script

--- bench.sh
+++ bench.sh
@@ -9,29 +9,35 @@ GOEXE="${GOEXE-go}"
 # - Do the same for master
 # - then compare the two runs with benchcmp
 
-benchFilter=".*"
-
-if (( $# < 2 ));
+if (( $# < 1 ));
   then
     echo "USAGE: ./bench.sh <git-branch> <package-to-bench> (and <benchmark filter> (regexp, optional))"
     exit 1
 fi
 
+if [ $# -eq 1 ]; then
+  PACKAGE=./...
+  PACKAGEFILE="all"
+else
+  PACKAGE=./"$2"
+  PACKAGEFILE="$2"
+fi
 
 
 if [ $# -eq 3 ]; then
   benchFilter=$3
+else
+  benchFilter="."
 fi
 
 
-BRANCH=$1
-PACKAGE=$2
+BRANCH="$1"
+BRANCHFILE="$(echo "$1" | sed 's/\//-/g')"
 
-git checkout $BRANCH
-"${GOEXE}" test -test.run=NONE -bench="$benchFilter" -test.benchmem=true ./$PACKAGE > /tmp/bench-$PACKAGE-$BRANCH.txt
+git checkout "$BRANCH"
+"${GOEXE}" test -test.run=NONE -bench="$benchFilter" -test.benchmem=true "$PACKAGE" > "/tmp/bench-$PACKAGEFILE-$BRANCHFILE.txt"
 
 git checkout master
-"${GOEXE}" test -test.run=NONE -bench="$benchFilter" -test.benchmem=true ./$PACKAGE > /tmp/bench-$PACKAGE-master.txt
+"${GOEXE}" test -test.run=NONE -bench="$benchFilter" -test.benchmem=true "$PACKAGE" > "/tmp/bench-$PACKAGEFILE-$BRANCHFILE-master.txt"
 
-
-benchcmp /tmp/bench-$PACKAGE-master.txt /tmp/bench-$PACKAGE-$BRANCH.txt
+benchstat -split xxx "/tmp/bench-$PACKAGEFILE-master.txt" "/tmp/bench-$PACKAGEFILE-$BRANCHFILE.txt"
