gohugoio/hugo

hugolib: Fix Output format (AMP) render hooks is not map correctly

hugolib/content_map_page.go
@@ -22,6 +22,7 @@ import (
 	"sync"
 
 	"github.com/gohugoio/hugo/common/maps"
+	"github.com/gohugoio/hugo/output"
 
 	"github.com/gohugoio/hugo/common/types"
 	"github.com/gohugoio/hugo/resources"
@@ -199,7 +200,7 @@ func (m *pageMap) newPageFromContentNode(n *contentNode, parentBucket *pagesMapB
 
 			// Create a content provider for the first,
 			// we may be able to reuse it.
-			if i == 0 {
+			if po.f.Name == output.HTMLFormat.Name || po.f.Name == output.AMPFormat.Name {
 				contentProvider, err := newPageContentOutput(ps, po)
 				if err != nil {
 					return nil, err

neohugo/neohugo

hugolib/content_map_page.go
@@ -22,6 +22,7 @@ import (
 	"sync"
 
 	"github.com/neohugo/neohugo/common/maps"
+	"github.com/neohugo/neohugo/output"
 
 	"github.com/neohugo/neohugo/common/types"
 	"github.com/neohugo/neohugo/resources"
@@ -199,7 +200,7 @@ func (m *pageMap) newPageFromContentNode(n *contentNode, parentBucket *pagesMapB
 
 			// Create a content provider for the first,
 			// we may be able to reuse it.
-			if i == 0 {
+			if po.f.Name == output.HTMLFormat.Name || po.f.Name == output.AMPFormat.Name {
 				contentProvider, err := newPageContentOutput(ps, po)
 				if err != nil {
 					return nil, err